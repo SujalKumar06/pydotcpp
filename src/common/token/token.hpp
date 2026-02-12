562fa2b77836114dba7761605cd9bb1cc31f7483
Siddharth Maira <maira.siddharth@gmail.com>
2026-02-12 22:07:52 +0530

    filled token.hpp and lexer.hpp with basic declarations(from ankit's commit)

--- src/common/token/token.hpp
+++ src/common/token/token.hpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 
 #include <string>
@@ -80,3 +81,62 @@ public:
     // This constructor will allow us to easily make the tokens while coding
     Token(TokenType type, std::string val, int l, int c);
 };
+=======
+#pragma once
+
+#include <string>
+
+enum class TokenType {
+    DEF,
+    RETURN,
+    IF,
+    ELSE,
+    FOR,
+    WHILE,
+
+    IDENTIFIER,
+    NUMBER,
+    STRING,
+
+    PLUS, // +
+    MINUS, // -
+    STAR, // *
+    SLASH, // /
+    ASSIGN, // =
+    MODULO, // %
+    GREATERTHAN, // >
+    LESSERTHAN, // <
+
+    LPAREN, // (
+    RPAREN, // )
+
+    NEWLINE,
+    EOF_TOKEN,
+
+    COMMA, // ,
+    COLON, // :
+    INDENT,
+    DEDENT,
+    COMMENT // #
+};
+
+
+class Token{
+public:
+    // Main part of a token
+    TokenType type;
+    std::string value;
+
+    // Used to tell user about error in case error is found
+    int line;
+    int column;
+
+    // This constructor will allow us to easily make the tokens while coding
+    Token(TokenType type, std::string val, int l, int c)
+};
+
+
+
+
+
+>>>>>>> b754426 (filled token.hpp and lexer.hpp with basic declarations(from ankit's commit))
