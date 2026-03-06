9120e6d349bac1a8d64835110e109abee44f8b82
Siddharth Maira <maira.siddharth@gmail.com>
2026-03-06 19:21:20 +0530

    formatted

--- src/parser/stmtparser.cpp
+++ src/parser/stmtparser.cpp
@@ -1,6 +1,8 @@
+#include "stmtparser.hpp"
+
 #include <vector>
 
 #include "token.hpp"
-#include "stmtparser.hpp"
 
-StmtParser::StmtParser(std::vector<Token> tokens) : tokens(std::move(tokens)), exprparser(this->tokens, index) {}
+StmtParser::StmtParser(std::vector<Token> tokens)
+    : tokens(std::move(tokens)), exprparser(this->tokens, index) {}
