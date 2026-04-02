64aa36d2cbc78bfc3a154df2916a9aae6aa90a00
Sujal Kumar <sujaljayantkumar06@gmail.com>
2026-04-02 23:33:58 +0530

    Updated the statement parser files

--- src/parser/stmtparser.cpp
+++ src/parser/stmtparser.cpp
@@ -6,3 +6,78 @@
 
 StmtParser::StmtParser(std::vector<Token> tokens)
     : tokens(std::move(tokens)), exprparser(this->tokens, index) {}
+
+Token StmtParser::peek() {
+    if (index < tokens.size()) {
+        return tokens[index];
+    } else {
+        return tokens.back();  // EOF Token
+    }
+}
+
+Token StmtParser::peekNext() {
+    if (index + 1 < tokens.size()) {
+        return tokens[index + 1];
+    } else {
+        return tokens.back();  // EOF Token
+    }
+}
+
+void StmtParser::advance() {
+    if (index < tokens.size()) {
+        index++;
+    }
+}
+
+std::unique_ptr<ProgramNode> StmtParser::parseProgram() {
+    auto program = std::make_unique<ProgramNode>();
+
+    while (peek().type != TokenType::EOF_TOKEN) {
+        // skip empty lines
+        if (peek().type == TokenType::NEWLINE) {
+            advance();
+            continue;
+        }
+
+        program->statements.push_back(parseStatement());
+    }
+
+    return program;
+}
+
+std::unique_ptr<ASTStmtNode> StmtParser::parseStatement() {
+    if (peek().type == TokenType::PRINT) {
+        return parsePrintStatement();
+    } else if (peek().type == TokenType::IF) {
+        return parseIfStatement();
+    } else if (peek().type == TokenType::WHILE) {
+        return parseWhileStatement();
+    } else if (peek().type == TokenType::FOR) {
+        return parseForStatement();
+    } else if (peek().type == TokenType::IDENTIFIER && peekNext().type == TokenType::ASSIGN) {
+        return parseVarDeclaration();
+    } else {
+        ignoreExpressionStatement();  // fallback
+        return parseStatement();
+    }
+}
+
+std::unique_ptr<ASTStmtNode> StmtParser::parseVarDeclaration() {
+    Token nameToken = peek();
+    advance();  // variable name
+    advance();  // =
+
+    auto value = exprparser.parseExpr();  // RHS
+
+    if (peek().type == TokenType::NEWLINE)
+        advance();
+
+    auto nameNode = std::make_unique<ReferenceNode>(nameToken.value);
+
+    // return the node
+    return std::make_unique<VarDeclNode>(std::move(nameNode), std::move(value));
+}
+
+// TODO: Think if just ignoring an expression statement is ok,
+// how to make it better
+void StmtParser::ignoreExpressionStatement() {}
--- src/parser/stmtparser.hpp
+++ src/parser/stmtparser.hpp
@@ -1,35 +1,35 @@
 #pragma once
 
-#include <vector>
 #include <memory>
+#include <vector>
 
-#include "token.hpp"
 #include "aststmt.hpp"
 #include "exprparser.hpp"
+#include "token.hpp"
 
 class StmtParser {
-public:
+   public:
     StmtParser(std::vector<Token> tokens);
 
     std::unique_ptr<ProgramNode> parseProgram();
 
-private:
-    std::vector<Token> tokens;  //the tokens vector that is returned by the lexer
-    int index = 0; //where are we in the token's vector
+   private:
+    std::vector<Token> tokens;  // the tokens vector that is returned by the lexer
+    int index = 0;              // where are we in the token's vector
 
     ExprParser exprparser;
 
-    //helpers
+    // helpers
     Token peek();
     Token peekNext();
     void advance();
 
-    //statement parsing
+    // statement parsing
     std::unique_ptr<ASTStmtNode> parseStatement();
     std::unique_ptr<ASTStmtNode> parsePrintStatement();
     std::unique_ptr<ASTStmtNode> parseVarDeclaration();
     std::unique_ptr<ASTStmtNode> parseIfStatement();
     std::unique_ptr<ASTStmtNode> parseWhileStatement();
     std::unique_ptr<ASTStmtNode> parseForStatement();
-    std::unique_ptr<ASTStmtNode> parseExpressionStatement();
+    void ignoreExpressionStatement();
 };
